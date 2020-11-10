1d9b298688520a5eee2aefd5fc09333ee37d9199
D3v <deviljin112@users.noreply.github.com>
2020-11-10 15:48:13 +0000

    Update test_factory.py

--- test_factory.py
+++ test_factory.py
@@ -10,7 +10,7 @@ class TestFactory(unittest.TestCase):
         self.assertTrue(self.tested_class.make_dough(True, True))
 
     def test_naan(self):
-        self.assertEqual(self.tested_class.bake_naan(["dough"]), "naan")
+        self.assertEqual(self.tested_class.bake_naan(True), "naan")
 
     def test_run(self):
         self.assertEqual(self.tested_class.run_factory(["water", "flour"]), "naan")
