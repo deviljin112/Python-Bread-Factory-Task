18ad73219371d89e0de7a9dfac3fbd396b7cac9e
D3v <deviljin112@users.noreply.github.com>
2020-11-10 16:22:11 +0000

    Spelling mistakes

--- test_factory.py
+++ test_factory.py
@@ -11,9 +11,9 @@ class TestFactory(unittest.TestCase):
     # Creates an instance of the BreadFactory class
     tested_class = BreadFactory()
 
-    # Creates test cases for each function
+    # Creates test case for each function in tested_class
     def test_dough(self):
-        # Checks if with specified input, the function returns the expected result
+        # Checks if with specified input in the function returns the expected result
         self.assertTrue(self.tested_class.make_dough(True, True))
 
     def test_naan(self):
